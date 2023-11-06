f29ff07d1bb4d13006227843dceeab8e991a021b
evgeny-chuvelev-sveasolar <evgeny.chuvelev@sveasolar.com>
2023-11-06 13:25:12 +0100

    Init commit

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { mount } from "marketing/MarketingApp";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref} />;
+};
