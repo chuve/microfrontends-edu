9995d9504fe64378f1a8ff2e1d38c630532b6be0
evgeny-chuvelev-sveasolar <evgeny.chuvelev@sveasolar.com>
2023-11-07 14:52:58 +0100

    Add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./Header";
 
 export default () => {
   return (
-    <div>
-      <h1>App</h1> <hr /> <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
